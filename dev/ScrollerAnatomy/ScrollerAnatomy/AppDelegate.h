c10c9761d60545f832e0010e029b2b215200d20c
Ahmed Farghaly <afarghaly@gmail.com>
2013-12-16 08:57:26 -0800

    Empty Sprite Kit project

---
+++ dev/ScrollerAnatomy/ScrollerAnatomy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ScrollerAnatomy
+//
+//  Created by Ahmed Farghaly on 12/16/13.
+//  Copyright (c) 2013 Ahmed Farghaly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ dev/ScrollerAnatomy/ScrollerAnatomy/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  ScrollerAnatomy
+//
+
+//  Copyright (c) 2013 Ahmed Farghaly. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
